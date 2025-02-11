ed01dc14bcbf11098fea919250a3163d206a2929
SHKAS <105296782+iiSHKAS@users.noreply.github.com>
2025-02-11 06:07:58 -0800

    تحديث التطبيقات - JS

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-// version: 1739282757
+// version: 1739282875
 let appsList = {
   "browsers": [
     {
@@ -35,6 +35,11 @@ let appsList = {
       "name": "Zen Browser",
       "id": "Zen-Team.Zen-Browser",
       "description": "متصفح يركز على البساطة والهدوء"
+    },
+    {
+      "name": "can u see me",
+      "id": "see.me",
+      "description": "can u see us"
     }
   ],
   "gamingAndEmulators": [
